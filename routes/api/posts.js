f79885c541729cc44fdbfd2361744aec2666ed58
Le Xuan Vinh <Xuanvinh9411@gmail.com>
2020-01-07 23:11:35 +0700

    Post like and unlike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,130 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 //@route    GET api/posts
 //@desc     Test route
-//@access   Public 
-router.get('/', (req, res) => res.send('Posts route'));
+//@access   Private 
+router.post('/', [auth, [
+    check('text', 'text is required')
+        .not()
+        .isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        })
+        const post = await newPost.save();
+
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.ststa(500).send('Server Error');
+    }
+
+});
+
+// @route   GET api/posts
+// @desc    Get api all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+// @route   GET api/posts/:id
+// @desc    Get api by ID
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+// @route   delete api/posts
+// @desc    delete a posts
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+        await post.remove();
+
+        res.json({ msg: 'Post remove success!' });
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === "ObjectId") {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post 
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.send(400).json({ msg: 'Post already liked' });
+        }
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post 
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.send(400).json({ msg: 'Post has not liked' });
+        }
+        const removeIndex = post.likes.map(like => like.user.toString().indexOf(req.user.id));
+        post.likes.splice(removeIndex, 1);
+        await post.save();
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
